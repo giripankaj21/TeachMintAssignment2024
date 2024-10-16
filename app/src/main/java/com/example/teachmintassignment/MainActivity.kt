5b4f2ba64b374cf218d7deaa0fbcc3245b81750f
giripankaj <giripankaj21@gmail.com>
2024-10-17 03:04:55 +0530

    MainActivity final Commit

--- app/src/main/java/com/example/teachmintassignment/MainActivity.kt
+++ app/src/main/java/com/example/teachmintassignment/MainActivity.kt
@@ -1,47 +1,32 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.teachmintassignment
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.teachmintassignment.ui.theme.TeachMintAssignmentTheme
 
 class MainActivity : ComponentActivity() {
+    private val repositoryViewModel: RepositoryViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TeachMintAssignmentTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    HomeScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        viewModel = repositoryViewModel
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TeachMintAssignmentTheme {
-        Greeting("Android")
     }
 }
